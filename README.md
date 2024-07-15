36bb41cbb90d4a2918c747dc5955540ed54461cb
maverik-io <josephchacko2006@gmail.com>
2024-07-15 16:48:31 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# joe-web-refresher
+
+### Setup
+1. first open up your command prompt and type this in:
+```bash
+pip install pyautogui
+```
+2. then go to the directory where the file `main.py` is located and type this in:
+```bash
+python main.py
+```
+3. then you should be good to go!
+
+## Problems
+- If you have any problems, tell me
+
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-import pygame as pg
-from sys import exit
+import webbrowser
+import time 
+import pyautogui
 
-pg.init()
-screen = pg.display.set_mode((400, 400))
+urls = [
+    "https://www.youtube.com",
+    "https://www.google.com",
+    "https://github.com/maverik-io/joe-web-refresher",
+]
 
-while True:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            exit()
+def open_urls(urls):
+    for url in urls:
+        webbrowser.open(url)
 
-    screen.fill('white')
 
-    pg.display.flip()
+def refresh_page():
+    pyautogui.hotkey('ctrl','tab')
+    time.sleep(1)
+    pyautogui.hotkey('ctrl', 'r')
+    time.sleep(1)
 
+def go_back():
+    pyautogui.hotkey('ctrl','shift','tab')
+
+
+if __name__ == "__main__":
+    open_urls(urls)
+    while True:
+        for i in range(len(urls)):
+            refresh_page()
+
+        for i in range(len(urls)):
+            go_back()
